c445782f0f7c661b400501cbdf8a73ea96705c99
andrewvt100 <andrewvt100@gmail.com>
2016-02-18 11:19:08 -0800

    Main menu implemented

--- budgettracker/main.cpp
+++ budgettracker/main.cpp
@@ -10,9 +10,105 @@
 
 using namespace std;
 
+void addExpenseMenu(void);
+void searchExpenseMenu(void);
+void mainMenu(void);
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    cout << "This has been changed"<<endl;
-    //Andrew changed this
+    
+    cout << "Welcome to LLT's budget program." << endl << endl;
+    
+    mainMenu();
+    
     return 0;
+}
+
+void mainMenu(void) {
+    
+    int userChoice;
+    bool userDone;
+    
+    while(!userDone) {
+        
+        cout << "Enter the corresponding number to select your choice of action:" << endl << "1: Add New Expense" << endl << "2: Search Expenses" << endl << "3: Exit Program" << endl << endl << "Your choice: ";
+        
+        cin >> userChoice;
+        
+        while (userChoice < 1|| userChoice > 3) {
+            cout << "Input invalid, please select again: " << endl;
+            cin >> userChoice;
+        }
+        
+        if (userChoice==1) {
+            addExpenseMenu();
+        }
+        
+        if (userChoice==2) {
+            searchExpenseMenu();
+        }
+        
+        if (userChoice==3) {
+            userDone = true;
+        }
+    }
+}
+
+void addExpenseMenu(void) {
+    
+    int userChoice;
+    bool userDone;
+    
+    while(!userDone) {
+        
+        cout << "Enter the corresponding number to select your choice of action:" << endl << "1: Add New Expense" << endl << "2: Search Expenses" << endl << "3: Exit Program" << endl << endl << "Your choice: ";
+        
+        cin >> userChoice;
+        
+        while (userChoice < 1|| userChoice > 3) {
+            cout << "Input invalid, please select again: " << endl;
+            cin >> userChoice;
+        }
+        
+        if (userChoice==1) {
+            addExpenseMenu();
+        }
+        
+        if (userChoice==2) {
+            searchExpenseMenu();
+        }
+        
+        if (userChoice==3) {
+            userDone = true;
+        }
+    }
+}
+
+void searchExpenseMenu(void) {
+    
+    int userChoice;
+    bool userDone;
+    
+    while(!userDone) {
+        
+        cout << "Enter the corresponding number to select your choice of action:" << endl << "1: Add New Expense" << endl << "2: Search Expenses" << endl << "3: Exit Program" << endl << endl << "Your choice: ";
+        
+        cin >> userChoice;
+        
+        while (userChoice < 1|| userChoice > 3) {
+            cout << "Input invalid, please select again: " << endl;
+            cin >> userChoice;
+        }
+        
+        if (userChoice==1) {
+            addExpenseMenu();
+        }
+        
+        if (userChoice==2) {
+            searchExpenseMenu();
+        }
+        
+        if (userChoice==3) {
+            userDone = true;
+        }
+    }    
 }
