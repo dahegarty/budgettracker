05d85c1683844f434cf78e420daf4ad308e0acbd
andrewvt100 <andrewvt100@gmail.com>
2016-02-18 10:29:36 -0800

    Initial Commit

---
+++ budgettracker/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  budgettracker
+//
+//  Created by Andrew van Tonningen on 2/18/16.
+//  Copyright © 2016 Landward Lemon Technologies. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
